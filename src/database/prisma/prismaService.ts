90d74b4c5c135a75fc3cf7c8fc39b3d2a9103345
Artag/Chris <scristxyz@gmail.com>
2024-11-05 15:43:41 -0500

    en el server remoto

---
+++ src/database/prisma/prismaService.ts
@@ -0,0 +1,71 @@
+import { PrismaClient } from '@prisma/client'
+import { buildLogger } from '../../config';
+import { SimpleSaveRequestDB } from '../../config/dto/simpleSaveRequestDB';
+
+export class PrismaService extends PrismaClient {
+  constructor () {
+    
+    super()
+    
+  }
+  logger = buildLogger(`prismaService.ts`)
+  async guardarRegistro(data: SimpleSaveRequestDB) {
+    const { name, lastName, email, document, documentType, reference, description, amount, status } = data;
+    //console.log(data);
+  
+    try {
+      const existingUser = await this.payer.findUnique({
+        where: {
+          email: email,
+        },
+      });
+  
+      let userId;
+      if (existingUser) {
+        const updatedUser = await this.payer.update({
+          where: {
+            id: existingUser.id,
+          },
+          data: {
+            name: name,
+            lastName: lastName,
+            document: document,
+            documentType: documentType,
+          },
+        });
+        userId = updatedUser.id;
+      } else {
+        const newUser = await this.prisma.create({
+          data: {
+            name: name,
+            lastName: lastName,
+            email: email,
+            document: document,
+            documentType: documentType,
+          },
+        });
+        userId = newUser.id;
+      }
+  
+      // Crear transacción
+      const transaccion = await this.transaction.create({
+        data: {
+          reference: reference,
+          description: description,
+          status: status,
+          amount: amount,
+          userId: userId,
+        },
+      });
+  
+      console.log('Transacción guardada:', transaccion);
+      return transaccion;
+  
+    } catch (error: any) {
+      const errorMessage = error.response?.data?.status?.message;
+      this.logger.log(`data:${JSON.stringify(errorMessage)}`);
+      throw error;
+    }
+  }
+  
+}
