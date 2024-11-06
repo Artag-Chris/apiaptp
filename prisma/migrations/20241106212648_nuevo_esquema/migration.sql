5b81014197b2a098891d27ea83c6477477adf909
Artag/Chris <scristxyz@gmail.com>
2024-11-06 16:37:52 -0500

    act

---
+++ prisma/migrations/20241106212648_nuevo_esquema/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE `Payer` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `lastName` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `document` VARCHAR(191) NOT NULL,
+    `documentType` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Payer_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Transaction` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `reference` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NOT NULL,
+    `amount` DECIMAL(65, 30) NOT NULL,
+    `payerId` INTEGER NOT NULL,
+    `fecha` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Transaction` ADD CONSTRAINT `Transaction_payerId_fkey` FOREIGN KEY (`payerId`) REFERENCES `Payer`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/database/prisma/prismaService.ts
+++ src/database/prisma/prismaService.ts
@@ -5,13 +5,13 @@ import { SimpleSaveRequestDB } from '../../config/dto/simpleSaveRequestDB';
 export class PrismaService extends PrismaClient {
   constructor () {
      
-    super()
-    
+    super()  
+    this.init(); 
   }
   logger = buildLogger(`prismaService.ts`)
   async guardarRegistro(data: SimpleSaveRequestDB) {
     const { name, lastName, email, document, documentType, reference, description, amount, status } = data;
-    //console.log(data);
+    console.log(data);
   
     try {
       const existingUser = await this.payer.findUnique({
@@ -63,9 +63,34 @@ export class PrismaService extends PrismaClient {
   
     } catch (error: any) {
       const errorMessage = error.response?.data?.status?.message;
-      this.logger.log(`data:${JSON.stringify(errorMessage)}`);
+     // this.logger.log(`data:${JSON.stringify(errorMessage)}`);
       throw error;
     }
   }
+
+
+  async init() {
+    try {
+      await this.$connect();
+      console.log(`Conexión a la base de datos establecida correctamente.`);
+    } catch (error) {
+      console.error('Error al conectar con la base de datos:', error);
+    }
+  }
+  async destroy() {
+    await this.$disconnect();
+  }
   
 }
+
+const prismaService = new PrismaService();
+
+process.on('SIGINT', async () => {
+  await prismaService.destroy();
+  process.exit(0);
+});
+
+process.on('SIGTERM', async () => {
+  await prismaService.destroy();
+  process.exit(0);
+});
